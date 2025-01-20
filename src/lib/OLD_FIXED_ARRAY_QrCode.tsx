8011171318d349c2be185da77867bde96b143e73
gdar463 <gdar463@gmail.com>
2025-01-20 16:49:47 +0100

    first try at qr

---
+++ src/lib/OLD_FIXED_ARRAY_QrCode.tsx
@@ -0,0 +1,31 @@
+export function OLD_FIXED_ARRAY_QrCode({ size }: { size?: number }) {
+  let bits: boolean[][] = [
+    [false, false, false, true],
+    [false, false, true, true],
+    [false, false, false, false],
+    [false, true, false, false],
+  ];
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${bits.length} ${bits.length}`}
+      height={size ?? bits.length}
+      width={size ?? bits.length}
+    >
+      {bits.map((yArr, y) => {
+        return yArr.map((value, x) => {
+          return (
+            <rect
+              key={`${x}-${y}`}
+              x={x}
+              y={y}
+              width={1}
+              height={1}
+              fill={value ? "#000000" : "#ffffff"}
+            />
+          );
+        });
+      })}
+    </svg>
+  );
+}
