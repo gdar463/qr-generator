d91efee2cea0abbc7a4cd028715ac961d50046cd
gdar463 <gdar463@gmail.com>
2025-01-25 12:41:38 +0100

    fixed eslint errors

--- src/lib/qrcode/qr.ts
+++ src/lib/qrcode/qr.ts
@@ -34,18 +34,18 @@ export default class QR extends Image {
   }
 
   detectContentType() {
-    if (this.content.match("^[0-9]+$")) {
+    if (new RegExp("^[0-9]+$").exec(this.content)) {
       return ContentTypeEnum.NUMERIC;
-    } else if (this.content.match("^[0-9A-Z \$%\*\+\-\.\/:]+$")) {
+    } else if (new RegExp("^[0-9A-Z $%*+\-./:]+$").exec(this.content)) {
       return ContentTypeEnum.ALPHANUMERIC;
     } else if (
       // This regex was copied from npmjs package "qrcode"
       // Repo: https://github.com/soldair/node-qrcode
       // License: MIT License
       // (license text can be found on the github repo)
-      this.content.match(
-        "^[\\u3000-\\u303F\\u3040-\\u309F\\u30A0-\\u30FF\\uFF00-\\uFFEF\\u4E00-\\u9FAF\\u2605-\\u2606\\u2190-\\u2195\\u203B\\u2010\\u2015\\u2018\\u2019\\u2025\\u2026\\u201C\\u201D\\u2225\\u2260\\u0391-\\u0451\\u00A7\\u00A8\\u00B1\\u00B4\\u00D7\\u00F7]+$",
-      )
+      new RegExp(
+        "^[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B\u2010\u2015\u2018\u2019\u2025\u2026\u201C\u201D\u2225\u2260\u0391-\u0451\u00A7\u00A8\u00B1\u00B4\u00D7\u00F7]+$",
+      ).exec(this.content)
     ) {
       return ContentTypeEnum.KANJI;
     }
