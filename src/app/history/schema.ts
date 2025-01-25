6709545c21e1db0c16709e6640caa4fb31aedb59
gdar463 <gdar463@gmail.com>
2025-01-25 11:12:03 +0100

    added history schema messages

--- src/app/history/schema.ts
+++ src/app/history/schema.ts
@@ -2,6 +2,13 @@ import { z } from "zod";
 
 export const historySchema = z.object({
   version: z.enum(["main", "fixed_array", "v0.1"]).default("main"),
-  qrVersion: z.coerce.number().min(1).max(40).default(1),
-  margin: z.coerce.number().min(0).default(0),
+  qrVersion: z.coerce
+    .number({ message: "Only numbers allowed" })
+    .min(1, "Version must be ranged between 1-40 (inclusive)")
+    .max(40, "Version must be ranged between 1-40 (inclusive)")
+    .default(1),
+  margin: z.coerce
+    .number({ message: "Only number allowed" })
+    .min(0, "Margin can't be lower than 0")
+    .default(0),
 });
