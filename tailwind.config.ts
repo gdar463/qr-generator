6e5d320a3a30d261237d4ce0a284d13bd02fe962
gdar463 <gdar463@gmail.com>
2025-02-06 12:58:38 +0100

    fixed fontFamily in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import fontFamily from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: "class",
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ["var(--font-geist-sans)", ...fontFamily.fontFamily.sans],
       },
       borderRadius: {
         lg: "var(--radius)",
